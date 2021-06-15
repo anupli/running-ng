510f21c28eb5cdd7975608bae086c09c81cac487
Zixian Cai <u5937495@anu.edu.au>
2021-06-15 21:53:27 +1000

    Flush stdout for runbms

--- running/runbms.py
+++ running/runbms.py
@@ -15,14 +15,14 @@ def run(args):
     for b in configuration.benchmarks:
         print("{} ".format(b.name), end="")
         for i in range(0, configuration.invocations):
-            print(i, end="")
+            print(i, end="", flush = True)
             for j, c in enumerate(configuration.configs):
                 jvm = configuration.jvms[c.split('|')[0]]
                 mods = [configuration.modifiers[x] for x in c.split('|')[1:]]
                 mod_b = b.attach_modifiers(mods)
                 if "PASSED" in mod_b.run(jvm):
-                    print(chr(ord('a')+j), end="")
+                    print(chr(ord('a')+j), end="", flush = True)
                 else:
-                    print(".", end="")
+                    print(".", end="", flush = True)
         print()
     return True
