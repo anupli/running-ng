b257ee182d408a4fa7f4c54e99118f1e64a0e17e
Zixian Cai <u5937495@anu.edu.au>
2021-09-27 12:13:07 +1000

    Add util for querying moma reservations

--- running/util.py
+++ running/util.py
@@ -1,10 +1,14 @@
-from typing import Any, List, TYPE_CHECKING, Tuple
+from typing import Any, List, TYPE_CHECKING, Optional, Tuple
 if TYPE_CHECKING:
     from running.config import Configuration
     from running.modifier import Modifier
     from running.runtime import Runtime
-
 import shlex
+import socket
+import urllib.request
+import enum
+import getpass
+from datetime import datetime
 
 
 def register(parent_class):
@@ -56,3 +60,49 @@ def smart_quote(_s: Any) -> str:
         return "\"{}\"".format(s)
     else:
         return s
+
+
+class MomaReservationStatus(enum.Enum):
+    NOT_RESERVED = 1
+    RESERVED_BY_OTHERS = 2
+    RESERVED_BY_ME = 3
+    NOT_MOMA = 4
+
+
+class MomaReservaton(object):
+    def __init__(self, status: MomaReservationStatus, user: Optional[str], start: Optional[datetime], end: Optional[datetime]):
+        self.status = status
+        self.user = user
+        self.start = start
+        self.end = end
+
+
+class Moma(object):
+    def __init__(self, host: Optional[str] = None):
+        if host is None:
+            self.host = socket.gethostname()
+            self.is_moma = socket.getfqdn().endswith(".moma")
+        else:
+            self.host = host
+            try:
+                self.is_moma = socket.gethostbyname_ex(
+                    self.host)[0].endswith(".moma")
+            except socket.gaierror:
+                self.is_moma = False
+        self.reserve_time_url = "http://10.0.0.1/reserve-time?host={}".format(
+            self.host)
+
+    def get_reservation(self) -> MomaReservaton:
+        if not self.is_moma:
+            return MomaReservaton(MomaReservationStatus.NOT_MOMA, None, None, None)
+        with urllib.request.urlopen(self.reserve_time_url) as response:
+            html = response.read()
+            if not html:
+                return MomaReservaton(MomaReservationStatus.NOT_RESERVED, None, None, None)
+            html = html.decode("utf-8")
+            user, start, end = html.split(",")
+            status = MomaReservationStatus.RESERVED_BY_ME if user == getpass.getuser(
+            ) else MomaReservationStatus.RESERVED_BY_OTHERS
+            start = datetime.fromtimestamp(int(start))
+            end = datetime.fromtimestamp(int(end))
+            return MomaReservaton(status, user, start, end)
