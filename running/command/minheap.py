c1bdc733e1456a675b86b4f92c8c01aa299bb425
Zixian Cai <u5937495@anu.edu.au>
2021-10-12 19:00:24 +1100

    Add persistence to minheap

--- running/command/minheap.py
+++ running/command/minheap.py
@@ -1,17 +1,22 @@
+from typing import Any, BinaryIO, Dict, Optional
 from running.config import Configuration
 from pathlib import Path
 from running.runtime import NativeExecutable, Runtime
 from running.benchmark import JavaBenchmark
 from running.suite import JavaBenchmarkSuite
-from running.util import parse_config_str
+from running.util import parse_config_str, config_str_encode
 import logging
 import tempfile
+import yaml
+from running.suite import is_dry_run
 
+configuration: Configuration
 
 def setup_parser(subparsers):
     f = subparsers.add_parser("minheap")
     f.set_defaults(which="minheap")
     f.add_argument("CONFIG", type=Path)
+    f.add_argument("RESULT", type=Path)
 
 
 def minheap_one_bm(suite: JavaBenchmarkSuite, runtime: Runtime, bm: JavaBenchmark, heap: int, minheap_dir: Path) -> float:
@@ -27,7 +32,7 @@ def minheap_one_bm(suite: JavaBenchmarkSuite, runtime: Runtime, bm: JavaBenchmar
         print(size_str, end="", flush=True)
         bm_with_heapsize = bm.attach_modifiers([heapsize])
         output, _ = bm_with_heapsize.run(runtime, timeout=timeout, cwd=minheap_dir)
-        if b"PASSED" in output:
+        if suite.is_passed(output):
             print(" o ", end="", flush=True)
             minh = mid
             hi = mid
@@ -42,25 +47,54 @@ def minheap_one_bm(suite: JavaBenchmarkSuite, runtime: Runtime, bm: JavaBenchmar
     return minh
 
 
+def run_with_persistence(result: Dict[str, Any], minheap_dir: Path, fd: Optional[BinaryIO]):
+    suites = configuration.get("suites")
+    maxheap = configuration.get("maxheap")
+    for suite_name, bms in configuration.get("benchmarks").items():
+        if suite_name not in result:
+            result[suite_name] = {}
+        suite = suites[suite_name]
+        for b in bms:
+            if b.name not in result[suite_name]:
+                result[suite_name][b.name] = {}
+            print("{}-{}".format(b.suite_name, b.name))
+            for c in configuration.get("configs"):
+                # skip a configuration if we have measured it
+                c_encoded = config_str_encode(c)
+                if c_encoded in result[suite_name][b.name]:
+                    continue
+                runtime, mods = parse_config_str(configuration, c)
+                if isinstance(runtime, NativeExecutable):
+                    logging.warning(
+                        "Minheap measurement not supported for NativeExecutable")
+                    pass
+                mod_b = b.attach_modifiers(mods)
+                minheap = minheap_one_bm(suite, runtime, mod_b, maxheap, minheap_dir)
+                print("minheap {}".format(minheap))
+                result[suite_name][b.name][c_encoded] = minheap
+                if fd:
+                    yaml.dump(result, fd)
+
 def run(args):
     if args.get("which") != "minheap":
         return False
+    global configuration
     configuration = Configuration.from_file(args.get("CONFIG"))
     configuration.resolve_class()
-    suites = configuration.get("suites")
-    maxheap = configuration.get("maxheap")
+    result_file = args.get("RESULT")
+    if result_file.exists():
+        with result_file.open() as fd:
+            result = yaml.safe_load(fd)
+            if result is None:
+                result = {}
+    else:
+        result = {}
     with tempfile.TemporaryDirectory(prefix="minheap-") as minheap_dir:
         logging.info("Temporary directory: {}".format(minheap_dir))
-        for suite_name, bms in configuration.get("benchmarks").items():
-            suite = suites[suite_name]
-            for b in bms:
-                print("{}-{}".format(b.suite_name, b.name))
-                for c in configuration.get("configs"):
-                    runtime, mods = parse_config_str(configuration, c)
-                    if isinstance(runtime, NativeExecutable):
-                        logging.warning(
-                            "Minheap measurement not supported for NativeExecutable")
-                    mod_b = b.attach_modifiers(mods)
-                    minheap = minheap_one_bm(suite, runtime, mod_b, maxheap, minheap_dir)
-                    print("minheap {}".format(minheap))
+        if is_dry_run():
+            run_with_persistence(result, minheap_dir, None)
+        else:
+            with result_file.open("w") as fd:
+                run_with_persistence(result, minheap_dir, fd)
+
     return True
