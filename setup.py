d3c56bbd11ec5ab65ff3f06abab280766009dbc4
Zixian Cai <u5937495@anu.edu.au>
2022-09-05 08:22:39 +0000

    Use the data specified in the manifest

--- setup.py
+++ setup.py
@@ -39,9 +39,6 @@ setuptools.setup(
 
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
-    package_data={
-        "running": ["data/**/*.yml"],
-    },
     include_package_data=True,
     python_requires=">=3.7",
 
